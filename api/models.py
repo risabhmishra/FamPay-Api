1bc7f3a8ffe63859cce73ca694e3ccd9f02e7d9b
risabh <risabh.mishra@sensennetworks.com>
2020-08-25 19:08:26 +0530

    Created a model class APIVideos for storing data from youtube search query

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class APIVideos(models.Model):
+    video_id = models.CharField(max_length=55, primary_key=True)
+    channel_id = models.CharField(max_length=55, blank=True, null=True)
+    title = models.CharField(max_length=255, blank=True, null=True)
+    description = models.CharField(max_length=255, blank=True, null=True)
+    published_at = models.DateTimeField(blank=True, null=True)
+    thumbnail = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
