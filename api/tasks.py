57cb710c48947620a17ebc70fc3b993003d17f7f
risabh <risabh.mishra@sensennetworks.com>
2020-08-25 19:05:51 +0530

    Added celery for Asynchrous Task Handling

---
+++ api/tasks.py
@@ -0,0 +1,45 @@
+import logging
+
+from api.models import APIVideos
+from FamPayAssignment.settings import DEVELOPER_KEY, YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, QUERY_TERM
+
+from celery.task.schedules import crontab
+from celery.decorators import periodic_task
+from googleapiclient.discovery import build
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+@periodic_task(
+    run_every=(crontab(minute='*/1')),
+    name="check_heartbeat",
+    ignore_result=True)
+def youtube_search():
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+                    developerKey=DEVELOPER_KEY)
+
+    # Call the search.list method to retrieve results matching the specified query term.
+    search_response = youtube.search().list(
+        q=QUERY_TERM,
+        part='id,snippet',
+        type='video',
+        order='date',
+        publishedAfter='2020-07-25T11:37:41.228849Z',
+    ).execute()
+
+    for search_result in search_response.get('items', []):
+        if search_result['id']['kind'] == 'youtube#video':
+            print(search_result)
+            try:
+                snippet = search_result['snippet']
+                video = APIVideos(video_id=search_result['id']['videoId'],
+                                  published_at=snippet['publishedAt'],
+                                  channel_id=snippet['channelId'],
+                                  title=snippet['title'],
+                                  description=snippet['description'],
+                                  thumbnail=snippet['thumbnails']['medium']['url'])
+                video.save()
+            except Exception as exception:
+                logger.exception(exception)
