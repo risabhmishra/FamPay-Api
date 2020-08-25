97a87695e13d128a576ee72bccc5244181f81ada
risabh <risabh.mishra@sensennetworks.com>
2020-08-25 19:14:02 +0530

    List all stored video data in a paginated response sorted in descending order of published datetime.

--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import filters
 
-# Create your views here.
+from api.serializers import APIVideosSerializer
+from api.models import APIVideos
+
+
+class APIVideosList(generics.ListCreateAPIView):
+    """
+    List all stored video data in a paginated response sorted in descending order of published datetime.
+    Query search text in the title and description field of APIVideos Model class.
+    """
+
+    search_fields = ['title', 'description']
+    filter_backends = (filters.SearchFilter,)
+    queryset = APIVideos.objects.all().order_by('-published_at')
+    serializer_class = APIVideosSerializer
